d6c40896711e7ba1c459ec7f36dcc61c7e3f3673
BenDeschand <77851535+BenDeschand@users.noreply.github.com>
2021-02-15 18:35:52 -0600

    multiScal done by Daniel Heller

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,8 @@
 //TODO
+// multiply each element of the row by the scalar value
+void matrix::multiScal(int scalar, int row) {
+
+   for(int i=0;i<col;i++) {
+     matrice[row][col] = matrice[row][col]*scalar; 
+   }
+}
