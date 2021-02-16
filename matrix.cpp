b766f5dbd63365ee2c5e8d770c5403b499332161
ChrisLoang <tloang2@uic.edu>
2021-02-15 19:14:57 -0600

    pivotNum, and one all in column

--- matrix.cpp
+++ matrix.cpp
@@ -5,6 +5,19 @@
 
 void Matrix::reduceCol(const int& col)
 {
+    //getting the pivot point from input Col
+    auto& pivotNum = matrice[0][col];
+
+    //iterate through the matrice divide by the pivot point
+    for(auto i = 0; i < row; i++)
+    {
+        for(auto j  = 0; j < col; j++)
+        {
+            (matrice[i][j] <= 0) ? : matrice[i][j] = matrice[i][j] / pivotNum; 
+
+        }
+    }
 
     
+
 }
